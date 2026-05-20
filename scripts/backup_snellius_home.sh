fc363a8c4a302e145008f591eaeb540070c9360f
jayh3 <yujie.jay.he@outlook.com>
2026-05-20 16:10:43 +0200

    Directory structure change

--- scripts/backup_snellius_home.sh
+++ scripts/backup_snellius_home.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rclone copy snellius:/home/yhe/RICH/snellius /data1/yujiehe/snellius-backup --exclude="/rich-data**" --log-file=/data1/yujiehe/snellius-logs/rclone-home.log  --log-file-max-size 1M  --log-file-max-backups 3
+rclone copy snellius:/home/yhe/snellius /data1/yujiehe/snellius-backup --exclude="/rich-data**" --log-file=/data1/yujiehe/snellius-logs/rclone-home.log  --log-file-max-size 1M  --log-file-max-backups 3
