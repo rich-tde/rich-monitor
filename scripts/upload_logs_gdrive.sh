4e54d94f861333c13572a4fadd3f9d2b14f87d16
jayh3 <yujie.jay.he@outlook.com>
2026-07-29 14:03:16 +0200

    Update the google drive sync to be both ways.

--- scripts/upload_logs_gdrive.sh
+++ scripts/upload_logs_gdrive.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-rclone copy /data1/yujiehe/snellius-logs/ gdrive:snellius-logs \
+rclone sync /data1/yujiehe/snellius-logs/ gdrive:snellius-logs \
     --exclude "*.log" \
     --exclude "*.json" \
+    --exclude "archived/**" \
     --log-file=/data1/yujiehe/snellius-logs/rclone-gdrive.log \
     --log-file-max-size 1M \
     --log-file-max-backups 3
